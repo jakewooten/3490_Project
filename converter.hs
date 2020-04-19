e6f8ad18edd633d9f1fd0e5f377f242bdd3b0119
Jake Wooten <wootendj@appstate.edu>
2020-04-19 01:43:47 -0400

    added a few new functions for markdown elements

--- converter.hs
+++ converter.hs
@@ -11,15 +11,27 @@ import Data.Char
 headerOne :: String -> String
 headerOne (x:xs)
     | take 7 (x:xs) == "#######" = x:xs
-    | take 6 (x:xs) == "######" = "<h6>" ++  xs ++ "<h6>"
-    | take 5 (x:xs) == "#####" = "<h5>" ++ xs ++ "<h5>"
-    | take 4 (x:xs) == "####" = "<h4>" ++ xs ++ "<h4>"
-    | take 3 (x:xs) == "###" = "<h3>" ++ xs ++ "<h3>"
-    | take 2 (x:xs) == "##" = "<h2>" ++ xs ++ "<h2>"
-    | otherwise = "<h1>" ++ xs ++ "<h1>"
+    | take 6 (x:xs) == "######" = "<h6>" ++ drop 5 xs ++ "</h6>"
+    | take 5 (x:xs) == "#####" = "<h5>" ++ drop 4 xs ++ "</h5>"
+    | take 4 (x:xs) == "####" = "<h4>" ++ drop 3 xs ++ "</h4>"
+    | take 3 (x:xs) == "###" = "<h3>" ++ drop 2 xs ++ "</h3>"
+    | take 2 (x:xs) == "##" = "<h2>" ++ drop 1 xs ++ "</h2>"
+    | otherwise = "<h1>" ++ xs ++ "</h1>"
 
 --this will be passed the indicating symbol (- or =) to determine what kind of header it will be
 headerTwo :: Char -> String -> String
 headerTwo x y 
-    | x == '=' = "<h1>" ++ y ++ "<h1>"
-    | x == '-' = "<h2>" ++ y ++ "<h2>"
+    | x == '=' = "<h1>" ++ y ++ "</h1>"
+    | x == '-' = "<h2>" ++ y ++ "</h2>"
+
+--function will be passes a string that will have the number of markers at the beginning and end of the line already checked
+bold :: String -> String
+bold (x:xs) = "<p><strong>" ++ drop 1 (init (init xs)) ++ "</p></strong>"
+
+italics :: String -> String
+italics (x:xs) = "<p><em>" ++ drop 1 (init xs) ++ "</p></em>"
+
+strike :: String -> String
+strike (x:xs) = "<p><s>" ++ drop 1 (init (init xs)) ++ "</p></s>"
+
+
